237b993315c0e2f36678348dc56f167f0994151a
Ankita Kumari <Ankitak28@gmail.com>
2021-02-09 21:40:57 +0530

    HomePage added

--- RhythmBox/app/src/main/java/com/example/rhythmbox/LoginActivity.java
+++ RhythmBox/app/src/main/java/com/example/rhythmbox/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,11 +25,12 @@ public class LoginActivity extends AppCompatActivity {
 
     private SignInButton signInButton;
     private GoogleSignInClient mGoogleSignInClient;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
-
+        progressDialog = new ProgressDialog(this);
         signInButton = findViewById(R.id.sign_in_button);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -37,11 +39,15 @@ public class LoginActivity extends AppCompatActivity {
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         if(account != null) {
             Log.d("TAG", "onCreate: already signed in");
+            Intent homePageIntent = new Intent(this,HomePage.class);
+            startActivity(homePageIntent);
             //Log.d("TAG", "onActivityResult: logged in with"+GoogleSignIn.getSignedInAccountFromIntent(mGoogleSignInClient.getSignInIntent()).getResult().getEmail());
         }else
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressDialog.setMessage("Signing Into your Google Account!");
+                progressDialog.show();
                 signIn();
             }
         });
@@ -55,9 +61,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        progressDialog.dismiss();
+        Log.d("TAG", "onActivityResult: resultcode "+resultCode);
         if(resultCode == 100){
+            Intent homePageIntent = new Intent(this,HomePage.class);
+            startActivity(homePageIntent);
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            Log.d("TAG", "onActivityResult: logged in with"+task.getResult().getEmail());
+            //Log.d("TAG", "onActivityResult: logged in with"+task.getResult().getEmail());
+            handleSignInResult(task);
         }
     }
 
@@ -65,6 +76,7 @@ public class LoginActivity extends AppCompatActivity {
         try{
             GoogleSignInAccount account = task.getResult(ApiException.class);
             Log.d("TAG", "handleSignInResult: signed in with ");
+            progressDialog.dismiss();
             //updateUI(account);
         } catch (ApiException e) {
             e.printStackTrace();

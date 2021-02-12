d3fa8f187cb4325bc79a1b0193ace105a0f404ac
Ankita Kumari <Ankitak28@gmail.com>
2021-02-13 00:55:58 +0530

    ViewPager implemented

--- RhythmBox/app/src/main/java/com/example/rhythmbox/HomePage.java
+++ RhythmBox/app/src/main/java/com/example/rhythmbox/HomePage.java
@@ -1,8 +1,18 @@
 package com.example.rhythmbox;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +20,47 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        initViewPager();
+    }
+    private void initViewPager(){
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new SongsFragment(),"Songs");
+        viewPagerAdapter.addFragments(new AlbumsFragments(),"Albums");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+    public class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+            fragments = new ArrayList<>();
+            titles = new ArrayList<>();
+        }
+        void addFragments(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
     }
 }

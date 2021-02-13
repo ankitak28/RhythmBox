41822ab161c4603aeaf5331b0925794430c64862
Ankita Kumari <Ankitak28@gmail.com>
2021-02-14 01:40:33 +0530

    Permission Added

--- RhythmBox/app/src/main/java/com/example/rhythmbox/HomePage.java
+++ RhythmBox/app/src/main/java/com/example/rhythmbox/HomePage.java
@@ -3,25 +3,57 @@ package com.example.rhythmbox;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
+import static android.Manifest.*;
+import static android.Manifest.permission.*;
+
 public class HomePage extends AppCompatActivity {
 
+    public static final int REQUEST_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        permissions();
         initViewPager();
     }
+    private void permissions(){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(HomePage.this,
+                    new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE){
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(HomePage.this,
+                        new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE);
+            }
+        }
+    }
+
     private void initViewPager(){
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         ViewPager viewPager = findViewById(R.id.viewpager);
